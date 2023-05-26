c84b7a48d3c94c1bbc254ba0a57e590e14c981b3
ArchanaShinde1 <Archana@Shinde2504@gmail.com>
2023-05-26 07:06:40 +0000

    call commmon functon for bazel cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,9 +16,8 @@
 # *****************************************************************
 set -vex
 
-#Clean up old bazel cache to avoid problems building TF
-bazel clean --expunge
-bazel shutdown
+source open-ce-common-utils.sh
+
 export BAZEL_LINKLIBS=-l%:libstdc++.a
 sh ${SRC_DIR}/oss_scripts/run_build.sh
 
@@ -26,5 +25,6 @@ sh ${SRC_DIR}/oss_scripts/run_build.sh
 # install using pip from the whl file
 pip install --no-deps $SRC_DIR/tensorflow_text*p${CONDA_PY}*.whl
 
-bazel clean --expunge
-bazel shutdown
+PID=$(bazel info server_pid)
+echo "PID: $PID"
+cleanup_bazel $PID
